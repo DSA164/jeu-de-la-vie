2050783bc7e7408faa039aac98e4227907b7f0eb
DSA164 <sacchet.d@gmail.com>
2025-08-20 15:42:46 +0200

    creation du module contenant le script par la gestion de la grille de jeu

---
+++ utils_grid.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------#
+#      Outils de gestion de la grille de jeu.     #
+#-------------------------------------------------#
+
+import numpy as np
+
+def grille(h: int = 128, v: int = 128):
+    return True
